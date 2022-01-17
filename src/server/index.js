2e0eb3abf6f240b5bf31ca8bb6c4c96974856d9f
Irfan Asif <hashtag.irfan@gmail.com>
2022-01-18 02:06:12 +0530

    production testing 4.0

--- src/server/index.js
+++ src/server/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 app.use(express.static('dist'));
 app.get('/api/getUsername', (req, res) => {
-  res.send({ username: 'Irfan Production test' });
+  res.send({ username: 'Irfan Production test @4' });
 });
 
 app.listen(process.env.PORT || 8080, () => {
