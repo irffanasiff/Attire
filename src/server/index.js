61a29b79d7e1c87c3a5fbf325cb84ca5e08eee3f
Irfan Asif <hashtag.irfan@gmail.com>
2022-01-18 00:46:27 +0530

    production testing

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: 'Irfan' }));
+app.get('/api/getUsername', (req, res) => res.send({ username: 'Irfan Production test' }));
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Listening on port ${process.env.PORT || 8080}!`);
