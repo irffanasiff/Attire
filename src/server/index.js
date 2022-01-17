27036998b9e396cf86bb9057cd0ea52ca6cb0084
Irfan Asif <hashtag.irfan@gmail.com>
2022-01-18 01:47:32 +0530

    production testing 2.0

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
 
 app.use(express.static('dist'));
-app.get('/api/getUsername', (req, res) => res.send({ username: 'Irfan Production test' }));
+app.get('/api/getUsername', (req, res) => {
+  res.send({ username: 'Irfan Production test' });
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Listening on port ${process.env.PORT || 8080}!`);
